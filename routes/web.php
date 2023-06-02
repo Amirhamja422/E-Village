b1f6d56f0cfcccbde345623515eef920c77253cb
Amirhamja422 <amirhamja422@gmail.com>
2023-06-03 03:43:04 +0600

    frontend start

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\frontend;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.home');
 });
+
+Route::get('/user/{id}', [frontend::class, 'index']);
+
